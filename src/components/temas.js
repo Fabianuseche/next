bd66e59e8cddbdd4494d3f798db835c04d9e3d0f
Fabianuseche <fabianusecherueda@gmail.com>
2023-05-21 17:29:47 -0500

    temelocastorage

--- src/components/temas.js
+++ src/components/temas.js
@@ -1,24 +1,29 @@
-
-
 import styles from "./temas.module.css"
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Temas() {
+  useEffect(() => {
+    const temaGuardado = localStorage.getItem('tema');
+    if (temaGuardado) {
+      document.body.classList.value = temaGuardado;
+    }
+  }, []);
 
-function handleTema(e){
-    document.body.classList.value =e.target.value  
-}
+  function handleTema(e) {
+    const temaSeleccionado = e.target.value;
+    document.body.classList.value = temaSeleccionado;
+    localStorage.setItem('tema', temaSeleccionado);
+  }
 
   return (
     <select className={styles.change} defaultValue="" onChange={handleTema}>
-
-   <option value="" style={{backgroundColor: 'white', color: 'black'}}>Color</option>
-   <option value="tema0" style={{backgroundColor: 'blue', color: 'white'}}>Azul</option>
-   <option value="tema1" style={{backgroundColor: 'lightgreen', color: 'white'}}>Verde</option>
-   <option value="tema2" style={{backgroundColor: 'orange', color: 'white'}}>Naranja</option>
-   <option value="tema3" style={{backgroundColor: 'pink', color: 'white'}}>Rosado</option>
-   <option value="tema4" style={{backgroundColor: 'black', color: 'white'}}>Oscuro</option>
-   <option value="tema5" style={{backgroundColor: 'red', color: 'white'}}>Rojo</option>
+      <option value="" style={{backgroundColor: 'white', color: 'black'}}>Color</option>
+      <option value="tema0" style={{backgroundColor: 'blue', color: 'white'}}>Azul</option>
+      <option value="tema1" style={{backgroundColor: 'lightgreen', color: 'white'}}>Verde</option>
+      <option value="tema2" style={{backgroundColor: 'orange', color: 'white'}}>Naranja</option>
+      <option value="tema3" style={{backgroundColor: 'pink', color: 'white'}}>Rosado</option>
+      <option value="tema4" style={{backgroundColor: 'black', color: 'white'}}>Oscuro</option>
+      <option value="tema5" style={{backgroundColor: 'red', color: 'white'}}>Rojo</option>
     </select>
   )
 }
