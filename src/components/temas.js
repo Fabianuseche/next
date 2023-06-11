2409c383dc5f8fad6e7b59fcf41c583b84b98934
Fabianuseche <fabianusecherueda@gmail.com>
2023-06-11 09:32:11 -0500

    colores

--- src/components/temas.js
+++ src/components/temas.js
@@ -1,7 +1,9 @@
-import styles from "./temas.module.css"
-import React, { useEffect } from 'react'
+import styles from "./temas.module.css";
+import React, { useEffect, useRef } from 'react';
 
 function Temas() {
+  const selectRef = useRef(null);
+
   useEffect(() => {
     const temaGuardado = localStorage.getItem('tema');
     if (temaGuardado) {
@@ -15,18 +17,31 @@ function Temas() {
     localStorage.setItem('tema', temaSeleccionado);
   }
 
+  function openOptions() {
+    selectRef.current.click();
+  }
+
   return (
-    <select className={styles.change} defaultValue="" onChange={handleTema}>
-      <option value="" style={{backgroundColor: 'white', color: 'black'}}>Tema</option>
-      <option value="tema0" style={{backgroundColor: 'blue', color: 'white'}}/>
-      <option value="tema1" style={{backgroundColor: 'lightgreen', color: 'white'}}/>
-      <option value="tema2" style={{backgroundColor: 'orange', color: 'white'}}/>
-      <option value="tema3" style={{backgroundColor: 'pink', color: 'white'}}/>
-      <option value="tema4" style={{backgroundColor: 'black', color: 'white'}}/>
-      <option value="tema5" style={{backgroundColor: 'red', color: 'white'}}/>
-      <option value="tema6" style={{backgroundColor: 'yellow', color: 'black'}}/>
-    </select>
-  )
+    <div className={styles.container}>
+      <div className={styles.selectBox} onClick={openOptions}>
+        <span className={styles.selectedOption}>Tema</span>
+        <select
+          ref={selectRef}
+          className={styles.change}
+          defaultValue=""
+          onChange={handleTema}
+        >
+          <option value="tema0" style={{backgroundColor: 'blue', color: 'white'}}>Blue</option>
+          <option value="tema1" style={{backgroundColor: 'lightgreen', color: 'white'}}>Light Green</option>
+          <option value="tema2" style={{backgroundColor: 'orange', color: 'white'}}>Orange</option>
+          <option value="tema3" style={{backgroundColor: 'pink', color: 'white'}}>Pink</option>
+          <option value="tema4" style={{backgroundColor: 'black', color: 'white'}}>Black</option>
+          <option value="tema5" style={{backgroundColor: 'red', color: 'white'}}>Red</option>
+          <option value="tema6" style={{backgroundColor: 'yellow', color: 'black'}}>Yellow</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
-export default Temas
+export default Temas;
