0945ab5f829e3a3ee61a7b42f9535ff6a860a33e
Fabianuseche <fabianusecherueda@gmail.com>
2023-06-12 19:46:44 -0500

    confirmar

--- src/components/event.js
+++ src/components/event.js
@@ -1,33 +1,37 @@
 import { updateEvent } from "@/pages/api/events";
-import dayjs from "dayjs"; // Importar la biblioteca dayjs para manipulación de fechas
-import customParseFormat from "dayjs/plugin/customParseFormat"; // Importar el plugin customParseFormat de dayjs
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { useRef, useState } from "react";
-import styles from "./events.module.css"; // Importar los estilos CSS para el componente
+import styles from "./events.module.css";
 
-
-dayjs.extend(customParseFormat); // Extender dayjs con el plugin customParseFormat
+dayjs.extend(customParseFormat);
 
 const Event = ({ id, name, date, lugar, hora, remove, update }) => {
-  const [updating, setUpdating] = useState(false)
+  const [updating, setUpdating] = useState(false);
   const formNombre = useRef();
   const formLugar = useRef();
   const formFecha = useRef();
   const formHora = useRef();
 
-  async function handleUpdateEvent(){
+  async function handleUpdateEvent() {
     await updateEvent(id, {
       name: formNombre.current.value,
       lugar: formLugar.current.value,
       date: formFecha.current.value,
       hora: formHora.current.value
-    })
-    setUpdating(false)
-    update()
+    });
+    setUpdating(false);
+    update();
   }
 
-  const daysDiff = datediff(date); // Calcular la diferencia de días utilizando la función 'datediff'
+  function confirmDelete() {
+    const confirmDelete = window.confirm('¿Estás seguro de que deseas borrar el evento?');
+    if (confirmDelete) {
+      remove();
+    }
+  }
 
-  // Mostrar mensaje personalizado si la diferencia de días es menor a 2
+  const daysDiff = datediff(date);
   let daysText;
   if (daysDiff === 0) {
     daysText = "Hoy";
@@ -37,14 +41,12 @@ const Event = ({ id, name, date, lugar, hora, remove, update }) => {
     daysText = `${daysDiff} `;
   }
 
-  // Mostrar mensaje adicional si la diferencia de días es menor o igual a 2
   let expiringSoon;
 
   if (daysDiff < 1) {
     expiringSoon = <div className={styles.expiring}>¡VENCE HOY!</div>;
   } else if (daysDiff < 2)
     expiringSoon = <div className={styles.expiring}>¡PROXIMO A VENCER!</div>;
-
   else {
     expiringSoon = null;
   }
@@ -60,75 +62,82 @@ const Event = ({ id, name, date, lugar, hora, remove, update }) => {
         )}
       </div>
       <div className={styles.eventname}>
-        {updating ? <input
-          type="text"
-          defaultValue={name}
-          ref={formNombre}
-          required
-          placeholder="Nombre del Evento"
-        /> : name}
+        {updating ? (
+          <input
+            type="text"
+            defaultValue={name}
+            ref={formNombre}
+            required
+            placeholder="Nombre del Evento"
+          />
+        ) : (
+          name
+        )}
       </div>
       <div className={styles.eventlugar}>
-        {updating ? <input
-          type="text"
-          defaultValue={lugar}
-          ref={formLugar}
-          required
-          placeholder="Lugar del Evento"
-        /> : lugar}
+        {updating ? (
+          <input
+            type="text"
+            defaultValue={lugar}
+            ref={formLugar}
+            required
+            placeholder="Lugar del Evento"
+          />
+        ) : (
+          lugar
+        )}
       </div>
       <div className={styles.eventdate}>
-        {updating ? <input
-          type="date"
-          defaultValue={dayjs(date).format("YYYY-MM-DD")}
-          ref={formFecha}
-          required
-          placeholder="Fecha del Evento"
-          min={dayjs().format("YYYY-MM-DD")}
-        /> : dayjs(date).format("YYYY-MM-DD")}
+        {updating ? (
+          <input
+            type="date"
+            defaultValue={dayjs(date).format("YYYY-MM-DD")}
+            ref={formFecha}
+            required
+            placeholder="Fecha del Evento"
+            min={dayjs().format("YYYY-MM-DD")}
+          />
+        ) : (
+          dayjs(date).format("YYYY-MM-DD")
+        )}
       </div>
       <div className={styles.eventhora}>
-        {updating ? <input
-          type="time"
-          defaultValue={hora}
-          ref={formHora}
-          required
-          placeholder="Hora del Evento"
-        /> : dayjs(hora, "HH:mm:ss").format("hh:mm a")}
-
+        {updating ? (
+          <input
+            type="time"
+            defaultValue={hora}
+            ref={formHora}
+            required
+            placeholder="Hora del Evento"
+          />
+        ) : (
+          dayjs(hora, "HH:mm:ss").format("hh:mm a")
+        )}
       </div>
       
       <div className={styles.actions}>
-  <div className={styles.buttonContainer}>
-    <button onClick={remove}>
-      Borrar
-    </button>
-    {
-      updating ?
-        <button onClick={handleUpdateEvent} className="update">
-          Guardar
-        </button> 
-        : <button onClick={() => setUpdating(true)}>
-          Editar
-        </button>
-    }
-    
-  </div>
-  
-</div>
-{expiringSoon}
+        <div className={styles.buttonContainer}>
+          <button onClick={confirmDelete}>Borrar</button>
+          {updating ? (
+            <button onClick={handleUpdateEvent} className="update">
+              Guardar
+            </button>
+          ) : (
+            <button onClick={() => setUpdating(true)}>Editar</button>
+          )}
+        </div>
+      </div>
+      {expiringSoon}
     </div>
-    
   );
-  
 };
 
 function datediff(d) {
-  var date1 = new Date(d); // Crear un objeto de fecha a partir de la cadena de fecha d
-  var date2 = new Date(); // Crear un objeto de fecha para la fecha actual
-  var difference = date1.getTime() - date2.getTime(); // Calcular la diferencia en milisegundos entre las dos fechas
-  var days = Math.ceil(difference / (1000 * 3600 * 24)); // Calcular la diferencia en días redondeando hacia arriba
-  return days; // Retornar la diferencia en días
+  var date1 = new Date(d);
+  var date2 = new Date();
+  var difference = date1.getTime() - date2.getTime();
+  var days = Math.ceil(difference / (1000 * 3600 * 24));
+  return days;
 }
 
-export default Event; // Exportar el componente Event como componente por defecto
+export default Event;
