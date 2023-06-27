05c20b201a9d80d077adc198124736079a5a7e5e
Fabianuseche <fabianusecherueda@gmail.com>
2023-06-27 10:48:44 -0500

    ver

--- src/pages/api/events.js
+++ src/pages/api/events.js
@@ -11,21 +11,36 @@ export async function createEvent(data) {
 export async function deleteEvent(id) {
   await db.from("events").where("id", id).del();
 }
-//************************************************************** */
+
 export async function updateEvent(id, data) {
   await db.from("events").where("id", id).update(data);
 }
 
-//***************************************************************** */
 export async function listEvent(user_id) {
-  const events = await db
-    .from("events")
-    .where({ user_id: user_id })
-    .orderBy("date")
-    .where(
-      db.raw("CURRENT_DATE < STR_TO_DATE(CONCAT(date, ' ', hora), '%Y-%m-%d %H:%i:%s')")
-    )
-    .select();
+  // Intentar obtener los eventos de la base de datos
+  try {
+    const events = await db
+      .from("events")
+      .where({ user_id: user_id })
+      .orderBy("date")
+      .where(
+        db.raw(
+          "CURRENT_DATE < STR_TO_DATE(CONCAT(date, ' ', hora), '%Y-%m-%d %H:%i:%s')"
+        )
+      )
+      .select();
 
-  return {events};
+    return { events };
+  } catch (error) {
+    // Si hay un error, intentar obtener los eventos del localStorage
+    const storedEvents = localStorage.getItem('events');
+    if (storedEvents) {
+      const events = JSON.parse(storedEvents);
+      return { events };
+    }
+
+    // Si no hay eventos en el localStorage, retornar un objeto vacío
+    return { events: [] };
+  }
 }
+
